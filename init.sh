4658a0210c86ee909e262d4e07caa4d6f5a91520
yumingvvv777 <sstm888@gmail.com>
2019-11-13 15:57:50 +0800

    change file

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+
+
+echo "############ 安装基础依赖 ############"
+
 apt-get update && apt-get install -y --no-install-recommends \
         cmake \
         git \
@@ -6,3 +12,26 @@ apt-get update && apt-get install -y --no-install-recommends \
         python-dev \
         python-pip \
         vim
+
+
+echo "############ 安装anaconda ############"
+
+curl -so /miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+&& chmod +x /miniconda.sh \
+&& /miniconda.sh -b -p /miniconda \
+&& rm /miniconda.sh
+
+PATH=/miniconda/bin:$PATH
+
+
+
+echo "############ 设置时区 ############"
+
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+
+echo "############ 设置编码 ############"
+
+LANG C.UTF-8 LC_ALL=C.UTF-8
+
+
